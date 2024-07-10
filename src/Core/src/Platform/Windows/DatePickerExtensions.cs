8e4bb9e75781696efadaa0796fee8079bc3a4930
Nanthini Mahalingam <nanthini.mahalingam@syncfusion.com>
2024-07-10 15:59:55 +0530

    updated comment

--- src/Core/src/Platform/Windows/DatePickerExtensions.cs
+++ src/Core/src/Platform/Windows/DatePickerExtensions.cs
@@ -99,7 +99,7 @@ namespace Microsoft.Maui.Platform
 
 		static readonly string[] BackgroundColorResourceKeys =
 		{
-			// Fixed the issue-22987
+			// Fixes-22987
 			"CalendarDatePickerBackground",
 			"CalendarDatePickerBackgroundPointerOver",
 			"CalendarDatePickerBackgroundPressed",
