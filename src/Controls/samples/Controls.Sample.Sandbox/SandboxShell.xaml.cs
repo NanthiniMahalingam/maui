0f671e099326630562c823e595236fc2b7cc7ad7
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-07-01 12:40:34 +0530

    dummy commit 1

--- src/Controls/samples/Controls.Sample.Sandbox/SandboxShell.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/SandboxShell.xaml.cs
@@ -7,3 +7,11 @@ public partial class SandboxShell : Shell
 		InitializeComponent();
 	}
 }
+
+
+
+
+
+
+// This file is part of the Maui.Controls.Sample project
+// It is subject to the license terms in the LICENSE file found in the top-level directory of
