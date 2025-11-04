2e2c954a79ee5b45d9d04703e65f178a3f9fb51a
NanthiniMahalingam <105482474+NanthiniMahalingam@users.noreply.github.com>
2025-11-04 17:30:47 +0530

    Added the test case

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue32223.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue32223 : _IssuesUITest
+{
+	public Issue32223(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "[Android] CollectionView items do not reorder correctly when using an item DataTemplateSelector";
+
+	[Test, Order(1)]
+	[Category(UITestCategories.CollectionView)]
+	public void CanReorderWithItemDataTemplateSelector_1()
+	{
+		App.WaitForElement("ReorderableCollectionView");
+		App.DragAndDrop("David", "Charlie");
+		VerifyScreenshot();
+	}
+
+	[Test, Order(2)]
+	[Category(UITestCategories.CollectionView)]
+	public void CanReorderWithItemDataTemplateSelector_2()
+	{
+		App.WaitForElement("ReorderableCollectionView");
+		App.DragAndDrop("David", "Alice");
+		VerifyScreenshot();
+	}
+
+	[Test, Order(3)]
+	[Category(UITestCategories.CollectionView)]
+	public void CanReorderWithItemDataTemplateSelector_3()
+	{
+		App.WaitForElement("ReorderableCollectionView");
+		App.DragAndDrop("Charlie", "Alice");
+		VerifyScreenshot();
+	}
+
+}
